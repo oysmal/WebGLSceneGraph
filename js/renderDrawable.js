c09db7b2ce18d6a1bd6f4ad8b7c8a30bc9baf7ce
Endre Oma <endre.88.oma@gmail.com>
2015-09-21 12:00:21 +0200

    Shorten variable access pattern in renderDrawable

--- js/renderDrawable.js
+++ js/renderDrawable.js
@@ -5,13 +5,17 @@
 var renderDrawable = function(drawable) {
     "use strict";
 
-    gl.useProgram(drawable.drawInfo.programInfo.program);
-	gl.bindBuffer(gl.ARRAY_BUFFER, drawable.drawInfo.bufferInfo.buffer);
-	var vPosition = gl.getAttribLocation(drawable.drawInfo.programInfo.program, "vPosition");
+    var programInfo = drawable.drawInfo.programInfo;
+    var bufferInfo = drawable.drawInfo.bufferInfo;
+    var uniformInfo = drawable.drawInfo.uniformInfo;
+
+    gl.useProgram(programInfo.program);
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferInfo.buffer);
+	var vPosition = gl.getAttribLocation(programInfo.program, "vPosition"); // TODO: maybe move this into programInfo?
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
    	gl.enableVertexAttribArray(vPosition);
 
-    gl.uniformMatrix4fv(drawable.drawInfo.programInfo.worldMatLocation, false, flatten(drawable.worldMatrix));      // Pass the world matrix of the current object to the shader.
-    gl.uniform4fv(drawable.drawInfo.programInfo.colorLocation, new Float32Array(drawable.drawInfo.uniformInfo.color));
-    gl.drawArrays(gl.TRIANGLES, 0, drawable.drawInfo.bufferInfo.numVertices);
+    gl.uniformMatrix4fv(programInfo.worldMatLocation, false, flatten(drawable.worldMatrix));      // Pass the world matrix of the current object to the shader.
+    gl.uniform4fv(programInfo.colorLocation, new Float32Array(uniformInfo.color));
+    gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numVertices);
 };
