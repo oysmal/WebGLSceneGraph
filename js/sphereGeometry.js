c97ff0697a04e04033380f13631d3d6b75434b36
Endre Oma <endre.88.oma@gmail.com>
2015-09-29 19:19:31 +0200

    Make sphereGeometry generate vertex normals

--- js/sphereGeometry.js
+++ js/sphereGeometry.js
@@ -17,51 +17,59 @@ function generateSphere(numSphereStacks, numSphereSlices) {
     var sliceAngleStep = (Math.PI*2)/numSphereSlices;
 
     var points = [];
+    var normals = [];
     var texCoords = [];
 
+    // Set radius to 0.5 to generate a sphere with diameter 1.0
+    var radius = 0.5;
+
     for (var i=0; i<numSphereSlices; i++) {
-        points.push(vec4(0.0, 0.5, 0.0, 1.0));
-        points.push(getCoordinate((i+1)*sliceAngleStep, stackAngleStep, 0.5));
-        points.push(getCoordinate((i+0)*sliceAngleStep, stackAngleStep, 0.5));
+        points.push(vec4(0.0, radius, 0.0, 1.0));
+        points.push(getCoordinate((i+1)*sliceAngleStep, stackAngleStep, radius));
+        points.push(getCoordinate((i+0)*sliceAngleStep, stackAngleStep, radius));
 
         texCoords.push(vec2(1, 0));
         texCoords.push(vec2((i+1)/numSphereStacks, 1/numSphereSlices));
         texCoords.push(vec2((i+0)/numSphereStacks, 1/numSphereSlices));
 
-
         for (var j=1; j<numSphereStacks-1; j++) {
-            points.push(getCoordinate((i+0)*sliceAngleStep, stackAngleStep*(j+0), 0.5));
-            points.push(getCoordinate((i+1)*sliceAngleStep, stackAngleStep*(j+0), 0.5));
-            points.push(getCoordinate((i+1)*sliceAngleStep, stackAngleStep*(j+1), 0.5));
+            points.push(getCoordinate((i+0)*sliceAngleStep, stackAngleStep*(j+0), radius));
+            points.push(getCoordinate((i+1)*sliceAngleStep, stackAngleStep*(j+0), radius));
+            points.push(getCoordinate((i+1)*sliceAngleStep, stackAngleStep*(j+1), radius));
 
             texCoords.push(vec2((i+0)/numSphereStacks, (j+0)/numSphereSlices));
             texCoords.push(vec2((i+1)/numSphereStacks, (j+0)/numSphereSlices));
             texCoords.push(vec2((i+1)/numSphereStacks, (j+1)/numSphereSlices));
 
-            points.push(getCoordinate((i+0)*sliceAngleStep, stackAngleStep*(j+0), 0.5));
-            points.push(getCoordinate((i+1)*sliceAngleStep, stackAngleStep*(j+1), 0.5));
-            points.push(getCoordinate((i+0)*sliceAngleStep, stackAngleStep*(j+1), 0.5));
+            points.push(getCoordinate((i+0)*sliceAngleStep, stackAngleStep*(j+0), radius));
+            points.push(getCoordinate((i+1)*sliceAngleStep, stackAngleStep*(j+1), radius));
+            points.push(getCoordinate((i+0)*sliceAngleStep, stackAngleStep*(j+1), radius));
 
             texCoords.push(vec2((i+0)/numSphereStacks, (j+0)/numSphereSlices));
             texCoords.push(vec2((i+1)/numSphereStacks, (j+1)/numSphereSlices));
             texCoords.push(vec2((i+0)/numSphereStacks, (j+1)/numSphereSlices));
         }
 
-        points.push(vec4(0.0,-0.5,0.0,1.0));
-        points.push(getCoordinate((i+0)*sliceAngleStep, Math.PI-stackAngleStep, 0.5));
-        points.push(getCoordinate((i+1)*sliceAngleStep, Math.PI-stackAngleStep, 0.5));
+        points.push(vec4(0.0,-radius,0.0,1.0));
+        points.push(getCoordinate((i+0)*sliceAngleStep, Math.PI-stackAngleStep, radius));
+        points.push(getCoordinate((i+1)*sliceAngleStep, Math.PI-stackAngleStep, radius));
 
         texCoords.push(vec2(0, 0));
         texCoords.push(vec2((i+0)/numSphereStacks, 1 - 2/numSphereSlices));
         texCoords.push(vec2((i+1)/numSphereStacks, 1 - 2/numSphereSlices));
     }
 
-    var surfaceNormals = generateSurfaceNormals(points);
+    // Generate vertex normals
+    for (var i = 0; i < points.length; ++i) {
+        normals.push(normalize(vec3(points[i])));
+    }
+
+    //normals = generateSurfaceNormals(points);
 
     return {
         "numVertices": points.length,
         "points": points,
-        "normals": surfaceNormals,
+        "normals": normals,
         "texCoords": texCoords
     };
 }
@@ -82,5 +90,5 @@ function getCoordinate(longitude, latitude, radius) {
     var x = Math.cos(longitude) * Math.sin(latitude);
     var z = Math.sin(longitude) * Math.sin(latitude);
 
-    return scale(radius, vec4(x, y, z, 1.0));
+    return vec4(scale(radius, vec3(x, y, z)), 1.0);
 }
